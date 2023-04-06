797cc3912b8885bd8c856799f72c866abdfff95c
Rommel Rico <me@rommelrico.com>
2023-04-05 20:49:17 -0700

    :sparkles: Ch8: Adds circular slider animation

--- rn-app-examples/src/CircularSlider/lesson/Cursor.tsx
+++ rn-app-examples/src/CircularSlider/lesson/Cursor.tsx
@@ -1,27 +1,60 @@
 import * as React from "react";
-import { StyleSheet, View } from "react-native";
+import { StyleSheet } from "react-native";
 
 import { StyleGuide } from "../../components";
+import Animated, {
+  SharedValue,
+  useAnimatedGestureHandler,
+  useAnimatedStyle,
+} from "react-native-reanimated";
+import { PanGestureHandler } from "react-native-gesture-handler";
+import { canvas2Polar, polar2Canvas } from "react-native-redash";
 
 interface CursorProps {
   r: number;
   strokeWidth: number;
+  theta: SharedValue<number>;
 }
 
-export const Cursor = ({ strokeWidth }: CursorProps) => {
+export const Cursor = ({ r, strokeWidth, theta }: CursorProps) => {
+  const center = { x: r, y: r };
+  const style = useAnimatedStyle(() => {
+    const { x: translateX, y: translateY } = polar2Canvas(
+      {
+        theta: theta.value,
+        radius: r,
+      },
+      center
+    );
+    return {
+      transform: [{ translateX }, { translateY }],
+    };
+  });
+  const onGestureEvent = useAnimatedGestureHandler({
+    onActive: (event, ctx) => {
+      const { translationX, translationY } = event;
+      theta.value = canvas2Polar(
+        { x: translationX, y: translationY },
+        center
+      ).theta;
+    },
+  });
   return (
-    <View
-      style={[
-        {
-          ...StyleSheet.absoluteFillObject,
-          width: strokeWidth,
-          height: strokeWidth,
-          borderRadius: strokeWidth / 2,
-          borderColor: "white",
-          borderWidth: 5,
-          backgroundColor: StyleGuide.palette.primary,
-        },
-      ]}
-    />
+    <PanGestureHandler onGestureEvent={onGestureEvent}>
+      <Animated.View
+        style={[
+          {
+            ...StyleSheet.absoluteFillObject,
+            width: strokeWidth,
+            height: strokeWidth,
+            borderRadius: strokeWidth / 2,
+            borderColor: "white",
+            borderWidth: 5,
+            backgroundColor: StyleGuide.palette.primary,
+          },
+          style,
+        ]}
+      />
+    </PanGestureHandler>
   );
 };
