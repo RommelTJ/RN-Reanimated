3964d2407ce6728aa370268f145a912409f92413
Rommel Rico <me@rommelrico.com>
2023-04-06 20:58:59 -0700

    :sparkles: Ch8: Clamping values

--- rn-app-examples/src/CircularSlider/lesson/Cursor.tsx
+++ rn-app-examples/src/CircularSlider/lesson/Cursor.tsx
@@ -7,8 +7,13 @@ import Animated, {
   useAnimatedGestureHandler,
   useAnimatedStyle,
 } from "react-native-reanimated";
-import { PanGestureHandler } from "react-native-gesture-handler";
-import { canvas2Polar, polar2Canvas } from "react-native-redash";
+import {
+  PanGestureHandler,
+  PanGestureHandlerGestureEvent,
+} from "react-native-gesture-handler";
+import { canvas2Polar, clamp, polar2Canvas } from "react-native-redash";
+
+const THRESHOLD = 0.001;
 
 interface CursorProps {
   r: number;
@@ -30,14 +35,27 @@ export const Cursor = ({ r, strokeWidth, theta }: CursorProps) => {
       transform: [{ translateX }, { translateY }],
     };
   });
-  const onGestureEvent = useAnimatedGestureHandler({
+  const onGestureEvent = useAnimatedGestureHandler<
+    PanGestureHandlerGestureEvent,
+    {
+      offset: { x: number; y: number };
+    }
+  >({
     onStart: (event, ctx) => {
       ctx.offset = polar2Canvas({ theta: theta.value, radius: r }, center);
     },
     onActive: (event, ctx) => {
       const { translationX, translationY } = event;
       const x = ctx.offset.x + translationX;
-      const y = ctx.offset.y + translationY;
+      const y1 = ctx.offset.y + event.translationY;
+      let y: number;
+      if (x < r) {
+        y = y1;
+      } else if (theta.value < Math.PI) {
+        y = clamp(y1, 0, r - THRESHOLD);
+      } else {
+        y = clamp(y1, r, 2 * r);
+      }
       const value = canvas2Polar({ x, y }, center).theta;
       theta.value = value > 0 ? value : 2 * Math.PI + value;
     },
