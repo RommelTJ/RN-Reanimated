d5644459f12f681e54d6f05b5fd491897066b792
Rommel Rico <me@rommelrico.com>
2023-04-10 07:59:44 -0700

    :sparkles: ch11: Animating second and third cards

--- rn-app-examples/src/DynamicSpring/lesson/DynamicSpring.tsx
+++ rn-app-examples/src/DynamicSpring/lesson/DynamicSpring.tsx
@@ -1,5 +1,10 @@
 import { View, StyleSheet } from "react-native";
-import Animated, { useSharedValue } from "react-native-reanimated";
+import Animated, {
+  useAnimatedStyle,
+  useDerivedValue,
+  useSharedValue,
+  withSpring,
+} from "react-native-reanimated";
 
 import { Card, Cards, CARD_WIDTH, CARD_HEIGHT } from "../../components";
 
@@ -28,12 +33,35 @@ export const DynamicSpring = ({ width, height }: DynamicSpringProps) => {
     x: useSharedValue(0),
     y: useSharedValue(0),
   };
+
+  const translateX2 = useDerivedValue(() => withSpring(translate.x.value));
+  const translateY2 = useDerivedValue(() => withSpring(translate.y.value));
+  const style2 = useAnimatedStyle(() => {
+    return {
+      transform: [
+        { translateX: translateX2.value },
+        { translateY: translateY2.value },
+      ],
+    };
+  });
+
+  const translateX3 = useDerivedValue(() => withSpring(translateX2.value));
+  const translateY3 = useDerivedValue(() => withSpring(translateY2.value));
+  const style3 = useAnimatedStyle(() => {
+    return {
+      transform: [
+        { translateX: translateX3.value },
+        { translateY: translateY3.value },
+      ],
+    };
+  });
+
   return (
     <View style={styles.container}>
-      <Animated.View style={[styles.card]}>
+      <Animated.View style={[styles.card, style3]}>
         <Card card={Cards.Card3} />
       </Animated.View>
-      <Animated.View style={[styles.card]}>
+      <Animated.View style={[styles.card, style2]}>
         <Card card={Cards.Card2} />
       </Animated.View>
       <DraggableCard width={width} height={height} translate={translate} />
