631fd3a9e703aa6e386dffda6dca1c11c4ee00f9
Rommel Rico <me@rommelrico.com>
2023-04-11 20:26:31 -0700

    :sparkles: ch12: Positioning the cards in an absolute manner

--- rn-app-examples/src/DragToSort/lesson/SortableItem.tsx
+++ rn-app-examples/src/DragToSort/lesson/SortableItem.tsx
@@ -1,5 +1,36 @@
-import { View } from "react-native";
+import React, { ReactElement } from "react";
+import Animated, {
+  SharedValue,
+  useAnimatedGestureHandler,
+  useAnimatedStyle,
+} from "react-native-reanimated";
+import { PanGestureHandler } from "react-native-gesture-handler";
 
-export const SortableItem = () => {
-  return <View />;
+type Props = {
+  index: number;
+  offsets: { y: SharedValue<number> }[];
+  children: ReactElement;
+  width: number;
+  height: number;
+};
+export const SortableItem = (props: Props) => {
+  const { index, offsets, children, width, height } = props;
+  const currentOffset = offsets[index];
+  const style = useAnimatedStyle(() => {
+    return {
+      position: "absolute",
+      top: 0,
+      left: 0,
+      width,
+      height,
+      transform: [{ translateY: currentOffset.y.value }],
+    };
+  });
+  const onGestureEvent = useAnimatedGestureHandler({});
+
+  return (
+    <PanGestureHandler onGestureEvent={onGestureEvent}>
+      <Animated.View style={style}>{children}</Animated.View>
+    </PanGestureHandler>
+  );
 };
--- rn-app-examples/src/DragToSort/lesson/SortableList.tsx
+++ rn-app-examples/src/DragToSort/lesson/SortableList.tsx
@@ -1,12 +1,35 @@
 import type { ReactElement } from "react";
 import React from "react";
 import { ScrollView } from "react-native";
+import { SortableItem } from "./SortableItem";
+import { useSharedValue } from "react-native-reanimated";
 
 interface SortableListProps {
   children: ReactElement[];
   item: { width: number; height: number };
 }
 
-export const SortableList = ({ children }: SortableListProps) => {
-  return <ScrollView>{children}</ScrollView>;
+export const SortableList = ({ children, item }: SortableListProps) => {
+  const offsets = children.map((_, idx) => ({
+    y: useSharedValue(item.height * idx),
+  }));
+  return (
+    <ScrollView
+      contentContainerStyle={{ height: item.height * children.length }}
+    >
+      {children.map((child, idx) => {
+        return (
+          <SortableItem
+            key={idx}
+            index={idx}
+            offsets={offsets}
+            width={item.width}
+            height={item.height}
+          >
+            {child}
+          </SortableItem>
+        );
+      })}
+    </ScrollView>
+  );
 };
