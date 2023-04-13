f023edf5cb0932d1df23eb742ba23ac739d16b5f
Rommel Rico <me@rommelrico.com>
2023-04-12 20:15:42 -0700

    :sparkles: ch13: Wrapping things in PanGestureHandler

--- rn-app-examples/src/Bezier/lesson/ControlPoint.tsx
+++ rn-app-examples/src/Bezier/lesson/ControlPoint.tsx
@@ -1,9 +1,17 @@
 import Animated, {
   SharedValue,
+  useAnimatedGestureHandler,
   useAnimatedStyle,
 } from "react-native-reanimated";
+import {
+  PanGestureHandler,
+  PanGestureHandlerGestureEvent,
+} from "react-native-gesture-handler";
+import { onGestureEvent } from "react-native-redash/lib/typescript/v1";
 
 export const CONTROL_POINT_RADIUS = 20;
+
+type Offset = { x: number; y: number };
 
 interface ControlPointProps {
   x: SharedValue<number>;
@@ -13,21 +21,43 @@ interface ControlPointProps {
 }
 
 export const ControlPoint = ({ x, y }: ControlPointProps) => {
+  const onGestureEvent = useAnimatedGestureHandler<
+    PanGestureHandlerGestureEvent,
+    Offset
+  >({
+    onStart: (_, context) => {
+      context.x = x.value;
+      context.y = y.value;
+    },
+    onActive: (event, context) => {
+      const { translationY, translationX } = event;
+      x.value = context.x + translationX;
+      y.value = context.y + translationY;
+    },
+    onEnd: (event, context) => {},
+  });
+
   const style = useAnimatedStyle(() => {
     return {
-      transform: [{ translateX: x.value }, { translateY: y.value }],
+      transform: [
+        { translateX: x.value - CONTROL_POINT_RADIUS },
+        { translateY: y.value - CONTROL_POINT_RADIUS },
+      ],
     };
   });
   return (
-    <Animated.View
-      style={[
-        {
-          position: "absolute",
-          width: CONTROL_POINT_RADIUS * 2,
-          height: CONTROL_POINT_RADIUS * 2,
-        },
-        style,
-      ]}
-    />
+    <PanGestureHandler onGestureEvent={onGestureEvent}>
+      <Animated.View
+        style={[
+          {
+            position: "absolute",
+            width: CONTROL_POINT_RADIUS * 2,
+            height: CONTROL_POINT_RADIUS * 2,
+            // backgroundColor: "rgba(100, 200, 300, 0.5)",
+          },
+          style,
+        ]}
+      />
+    </PanGestureHandler>
   );
 };
